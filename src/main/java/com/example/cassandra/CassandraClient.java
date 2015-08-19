a40c04896edfeea0f3ca537dbb62ca9a2ca50d4c
kmofokeng <kmofokeng@deloitte.co.za>
2015-08-19 11:24:48 +0200

    Initial commit - Cassandra DB Sample Code

---
+++ src/main/java/com/example/cassandra/CassandraClient.java
@@ -0,0 +1,16 @@
+package com.example.cassandra;
+
+/**
+ * Created by kmofokeng on 18/08/2015.
+ */
+public class CassandraClient {
+    
+    public static void main(String[] args) {
+        CassandraConnector client = new CassandraConnector();
+        client.connect();
+        client.createSchema();
+        client.loadData();
+        client.querySchema();
+        client.close();
+    }
+}
